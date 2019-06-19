21ab046fcf6ce1c628027386b3301cb00ff3270a
Austin Trow <austintrow@gmail.com>
2019-06-19 19:32:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
     return "Your shopping cart is empty.";
   } else {
     let keys = Object.keys(cart);
-    console.log(keys);ß
+    console.log(keys);
     if (keys.length === 1) {
       return `In your cart, you have ${keys[0]} at \$${cart[keys]}.`;
     }
